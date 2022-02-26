14911fcf20804ff890a438b10b82bb61843b6ed3
Charles Spencer Blancas <charles.blancas@mail.mcgill.ca>
2022-02-25 21:11:10 -0500

    Temporary changes, just to stash

---
+++ gamehub.py
@@ -0,0 +1,38 @@
+import os
+import random
+import discord
+from discord.ext import commands
+
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+token = os.getenv("DISCORD_TOKEN")
+bot = commands.Bot(command_prefix='$')
+client = discord.Client()
+
+print(type(token))
+
+
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord as a bot!')
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith("$talk"):
+        await message.channel.send("Bot is talking! my-PC")
+
+
+@bot.command()
+async def test(ctx, arg):
+    await ctx.send(arg)
+    pass
+
+
+bot.run(token)
+client.run(token)

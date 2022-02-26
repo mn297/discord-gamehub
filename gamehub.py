2c91159753756c5860012af77bcf817b2db0e121
asmithrsa <84108607+asmithrsa@users.noreply.github.com>
2022-02-25 21:36:40 -0500

    Update gamehub.py

--- gamehub.py
+++ gamehub.py
@@ -1,5 +1,5 @@
 import os
-from rockpaperscissors import rockpaperscissorscomputer
+import random
 import discord
 from dotenv import load_dotenv
 from discord.ext import commands
@@ -8,5 +8,19 @@ load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 client = discord.Client()
 bot = commands.Bot(command_prefix='$')
+@bot.command(name='rps rock')
+async def rpsrock(ctx):
+
+    com = random.randrange(1,3)
+    if com == 1:
+        out = "Tie"
+    elif com == 2:
+        out = "I choose scissors. You win!"
+    elif com == 3:
+        out = "I choose paper. I win!"
+        
+    await ctx.send(out)
+    
+    
 client.run(TOKEN)
 bot.run(TOKEN)

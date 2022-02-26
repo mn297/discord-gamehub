b4dcde68756251b9f272b1595bfa45631381e8d3
asmithrsa <84108607+asmithrsa@users.noreply.github.com>
2022-02-25 20:53:22 -0500

    Create rockpaperscissors.py

---
+++ rockpaperscissors.py
@@ -0,0 +1,23 @@
+import random
+import discord
+import os
+from discord.ext import commands
+
+async def rockpaperscissorscomputer(userin):
+    client = discord.Client()
+    bot = commands.Bot(command_prefix='$')
+
+    @bot.command(name='rps rock')
+    async def rockpaperscissorscomputer(ctx):
+
+        com = random.randrange(1,3)
+        if com == 1:
+            print("Tie!")
+        elif com == 2:
+            print("I choose scissors. You win!")
+        elif com == 3:
+            print("I choose paper. I win!")
+        
+        await ctx.send(com)
+
+   
